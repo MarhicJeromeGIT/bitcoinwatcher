74008f8bf31e58b429cfcb4997036e87b98036f2
marhicjeromegit <marhic.jerome@gmail.com>
2017-06-03 18:14:30 +0000

    matrix style transactions view with canvas

--- app/javascript/packs/bitcoin_socket.jsx
+++ app/javascript/packs/bitcoin_socket.jsx
@@ -11,14 +11,56 @@ const Value = props => (
   <div>Value: {props.value} bitcoins!</div>
 )
 
+/* a single vertical, descending 'matrix style' line */
+class MatrixLine {
+  constructor(value, context){
+    this.value = value;
+    this.fontSize = 20;
+    this.ctx = context;
+    // How many columns we have
+    var colSize = this.fontSize * 1.5;
+    var colnum = this.ctx.canvas.width / colSize;
+    this.text = this.value + " BTC";
+    this.verticalSpacing = this.fontSize; // TODO: change size depending on value
+    // Randomly choose a column (todo: don't overlap strings)
+    var col = Math.floor(Math.random() * colnum);
+    this.x = col * colSize;
+    this.ylength = this.verticalSpacing * this.text.length;
+    this.y = -this.ylength;
+    this.speed = 50 * ( Math.random() * 0.5 + 0.5 );
+  }
+  
+  update(dt){
+    this.y += this.speed * dt;
+    return this.y;
+  }
+
+  draw(){
+    this.ctx.font = this.fontSize + 'px Arial';
+    
+    // 'matrix' style color gradient.
+    var grd = this.ctx.createLinearGradient(0,this.y,0,this.y + this.ylength);
+    grd.addColorStop(0,"green");
+    grd.addColorStop(1,"white");
+    this.ctx.fillStyle = grd;
+     
+    for (var i = 0; i < this.text.length; i++) {
+      this.ctx.fillText(this.text[i], this.x, this.y + i * this.verticalSpacing);
+    }
+  }
+}
+
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {values: [1,2,3]};
-    
+    this.state = {values: [], count: 0};
+    this.lines = new Array();
+
     // This binding is necessary to make `this` work in the callback
     this.onOpen = this.onOpen.bind(this);
     this.onMessage = this.onMessage.bind(this);
+    this.updateLines = this.updateLines.bind(this);
+    this.draw = this.draw.bind(this);
   }
 
   componentDidMount() {
@@ -30,10 +72,20 @@ class Welcome extends React.Component {
     this.websocket.onmessage = this.onMessage;
 
     this.timerID = setInterval(function(){ this.websocket.send('{"op":"ping"}'); }, 30000);
+    this.updateTimerId = setInterval( () => this.updateLines(0.066), 66);
+    this.drawTimerId = setInterval( () => this.draw(), 66);
+    
+    // Initialize the canvas
+    var c = document.getElementById("matrix");
+    this.ctx = c.getContext("2d");
+    this.ctx.canvas.width  = window.innerWidth;
+    this.ctx.canvas.height = window.innerHeight;
   }
   
   componentWillUnmount() {
     clearInterval(this.timerID);
+    clearInterval(this.updateTimerId);
+    clearInterval(this.drawTimerId);
   }
   
   onOpen(evt){
@@ -47,13 +99,16 @@ class Welcome extends React.Component {
     var data = JSON.parse(evt.data);
     var value = data['x']['out'][0].value;
     value = value / Math.pow(10,8);
-    console.log(value);
+    //console.log(value);
     
+    this.lines.push( new MatrixLine(value, this.ctx) );
     var values = this.state.values;
     values.unshift(value);
+    // Only keep the 10 most recent transactions
     values = values.slice(0,10);
     this.setState({
-      values: values
+      values: values,
+      count: this.state.count + 1,
     });
     
     this.state.values.push(value);
@@ -64,15 +119,39 @@ class Welcome extends React.Component {
     clearInterval(this.timerID);
     this.websocket.close();
   }
-    
+  
+  updateLines(dt){
+    var toRemove = new Array();
+    console.log(this.lines.length + "elements");
+    this.lines.forEach(function(element,index) {
+      // Update line position and delete it when it goes out of the frame.
+      var y = element.update(dt);
+      if(y > this.ctx.canvas.height){
+        toRemove.push(index);
+      }
+    }, this);
+
+    toRemove.forEach( function(element){
+      this.lines.splice(element,1);
+    }, this);
+  }
+
   render() {
     return <div>
-      <h1>Hello, {this.props.name}</h1>
+      <h1>{this.state.count} transactions</h1>
       {this.state.values.map((value, index) =>
         <Value key={index.toString()}
                   value={value} />
       )}
     </div>
+  }
+  
+  draw(){
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    
+    for (var i = 0; i < this.lines.length; i++) {
+      this.lines[i].draw();
+    }
   }
 }
 
