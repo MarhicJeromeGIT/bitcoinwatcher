5b327e17739ad1ac16aa4380ed562665153d5694
marhicjeromegit <marhic.jerome@gmail.com>
2017-06-05 16:21:23 +0000

    cleaning up

--- app/javascript/packs/start_stop_button.jsx
+++ app/javascript/packs/start_stop_button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-export default class StartStopButton extends React.Component {
+export class StartStopButton extends React.Component {
   constructor(props){
     super(props);
   }
